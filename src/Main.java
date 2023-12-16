6f7e8084115e66002b5061f3a7c447c3a07af149
Helen <elena.kozovyakina@gmail.com>
2023-12-16 20:32:43 +0300

    change print

--- src/Main.java
+++ src/Main.java
@@ -284,7 +284,8 @@ public class Main {
 //
 //        System.out.println(plain);
 //        System.out.println("Hello");
-        System.out.println("Вносим изменения");
+//        System.out.println("Вносим изменения");
+        System.out.println("Вносим изменения в клонируемый проект");
     }
 
 //    public static byte[] encode(String pText, String pKey) {
